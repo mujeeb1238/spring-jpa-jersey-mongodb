ad02431c22d66eef6d7345c10e8786a8b17557be
mujeeb1238 <mujeeb.rahman.syed@gmail.com>
2015-07-04 11:07:26 +0000

    successfully integrated redis

--- src/main/java/com/beee/sample/api/BeeeResourceApi.java
+++ src/main/java/com/beee/sample/api/BeeeResourceApi.java
@@ -6,6 +6,7 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
 import com.beee.sample.services.BeeeMainService;
@@ -17,16 +18,29 @@ public class BeeeResourceApi {
 
 	
 	private final BeeeMainService service;
+	private final RedisTemplate<String, String> template;
+	
+	/*@Resource(name="template")
+    private ListOperations<String, String> listOps;
+	
+	@Resource(name="template")
+	private ValueOperations<String, Integer> intOp;*/
 	
 	@Autowired
-	public BeeeResourceApi(final BeeeMainService service) {
+	public BeeeResourceApi(final BeeeMainService service, RedisTemplate<String, String> template) {
 		this.service = service;
+		this.template = template;
 	}
 	
 	@GET
 	@Path("/sayhello")
 	public Response sayHello(){
-		
+		//listOps.leftPush("1238", "Syed Mujeeb ur Rahman");
+		 template.opsForValue().set("1238", "Syed Mujeeb ur Rahman opts for value11");
+        
+        // or use template directly
+        //redisTemplate.boundListOps("1238").leftPush("Hellow world");
+        //System.out.println(template.opsForValue().get("1238"));
 		return Response.status(Status.OK).entity(service.sayHello()).build();
 	}
 }
